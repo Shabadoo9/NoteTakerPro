d9aff5dd408d065a014e2f2361f2d57ee6fd7edc
Shabadoo9 <brinstonzbpe@gmail.com>
2023-10-13 13:12:38 -0400

    added app.use

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const path = require('path')
 
 const app = express();
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.json());
