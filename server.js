696ae88c778de3f77250102ae4edb6fc6f404acd
Shabadoo9 <brinstonzbpe@gmail.com>
2023-10-13 13:42:35 -0400

    added note api call

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 //Post api call to post notes
 app.post('/api/notes', async (req, res) => {
-    try{
+    try {
         const data = await fs.readFile(path.resolve(__dirname, './db/db.json'), 'utf8');
         const notes = JSON.parse(data);
 
@@ -25,24 +25,36 @@ app.post('/api/notes', async (req, res) => {
 
         res.json(userNote);
     } catch (err) {
-    console.log(err);
-    res.status(500).json({error: 'internal server error'})
+        console.log(err);
+        res.status(500).json({ error: 'internal server error' })
     }
 });
 
 // GET - API call for a note with a unique ID
 app.get('/api/notes/:id', async (req, res) => {
     try {
-      const data = await fs.readFile(path.resolve(__dirname, './db/db.json'), 'utf8');
-      const notes = JSON.parse(data);
-      const note = notes.find((note) => note.id === parseInt(req.params.id));
-      if (note) {
-        res.json(note);
-      } else {
-        res.status(404).json({ error: 'Note not found' });
-      }
+        const data = await fs.readFile(path.resolve(__dirname, './db/db.json'), 'utf8');
+        const notes = JSON.parse(data);
+        const note = notes.find((note) => note.id === parseInt(req.params.id));
+        if (note) {
+            res.json(note);
+        } else {
+            res.status(404).json({ error: 'Note not found' });
+        }
     } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal Server Error' });
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-  });
+});
+
+//get - API call for a note
+app.get('/api/notes/', async (req, res) => {
+    try {
+        const data = await fs.readFile(path.resolve(__dirname, ".db"), 'utf8');
+        const notes = JSON.parse(data);
+        res.json(notes);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
