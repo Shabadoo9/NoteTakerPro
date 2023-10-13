0048dc3abba7e348cd0300a0e513beaf2f1dc502
Shabadoo9 <brinstonzbpe@gmail.com>
2023-10-13 13:28:56 -0400

    added post api call

--- server.js
+++ server.js
@@ -9,3 +9,23 @@ const port = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
+
+//Post api call to post notes
+app.post('/api/notes', async (req, res) => {
+    try{
+        const data = await fs.readFile(path.resolve(__dirname, './db/db.json'), 'utf8');
+        const notes = JSON.parse(data);
+
+        const userNote = req.body;
+        userNote.id = math.floor(math.random() * 5000);
+
+        notes.push(userNote);
+
+        await fs.writeFile(path.resolve(__dirname, './db/db.json'), JSON.stringify(notes));
+
+        res.json(userNote);
+    } catch (err) {
+    console.log(err);
+    res.status(500).json({error: 'internal server error'})
+    }
+});
